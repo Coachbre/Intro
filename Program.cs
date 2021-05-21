90ea1ae6c476d40b63d14504306a1adb9d7797b0
Coachbre <brekonan1@gmail.com>
2021-05-21 15:27:38 -0400

    added starter code

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/*
+Console.Write("First Name> ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name> ");
+string lastName = Console.ReadLine();
+
+string initials = firstName[0] + " " + lastName[0];
+Console.WriteLine(initials);
+*/
+
+// for (int num = 10; num >= 1; num--) 
+// {
+//     Console.WriteLine(num);
+// }
+
+/* System.Console.Write("Who would you like to say hello to? ");
+
+string name = System.Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    System.Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    System.Console.WriteLine($"Hello, {name}!");
+}
+*/
+
+Console.Write("First Name> ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name> ");
+string lastName = Console.ReadLine();
+
+// Console.WriteLine($"{lastName} {firstName}");
+
+string reverse = "";
+// reverse set to empty string
+string fullName = firstName + " " + lastName;
+
+ for (int i = fullName.Length -1; i >=0; i--)
+ {
+     reverse += fullName[i]; /* reverse = reverse + fullName[i] */
+ }
+Console.WriteLine(reverse);
